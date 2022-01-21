4da18299d37d262fe1653dd6ad26b3bbe6d0f310
Christopher <christopher.black.2209@googlemail.com>
2022-01-21 03:04:31 +0000

    commit tree f1b69bc1982b27f95f63e0e67554d5fa89fe6a3a

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action) {
+    switch (action.type) {
+        case "ADD_FRIEND":
+            let moreFriends = [ ...state.friends, action.friend];
+            return {friends: moreFriends};
+        case "REMOVE_FRIEND":
+            let lessFriends = state.friends.filter(friend => friend.id !== action.id);
+            return {friends: lessFriends};
+        default:
+            return state; 
+    }
 }
